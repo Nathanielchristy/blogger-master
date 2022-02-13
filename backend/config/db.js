9eaa1f0dd2e9314f52b4324a621a3a91ded47067
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2022-02-14 02:51:24 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+//Connection file to mongo db
+import mongoose from "mongoose";
+import colors from "colors";
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://Nathaniel:gSYSi6hTyhniqPDX@cluster0.pvm4q.mongodb.net/Thirddata?retryWrites=true&w=majority"
+    , {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+export default connectDB;
